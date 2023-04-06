1e2ffb96155dd5178c1c129d6680168c8ca6000f
clapage <charles.lapage@gmail.com>
2023-04-06 23:49:11 +0000

    modified use state

--- compliment_backpack/src/components/AppNavbar.js
+++ compliment_backpack/src/components/AppNavbar.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const AppNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav>
-      <NavLink to="/signup">Sign Up</NavLink>
-      <NavLink to="/login">Login</NavLink>
+    <nav className="navbar">
+      <h1>
+        <Link to="/">Compliment Backpack</Link>
+      </h1>
+      <div className="navbar-links">
+        <button className="navbar-toggle" onClick={handleToggle}>
+          {isOpen ? 'Close' : 'Open'}
+        </button>
+        {isOpen && (
+          <div className="navbar-menu">
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Login</Link>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
--- compliment_backpack/src/components/SignUp.js
+++ compliment_backpack/src/components/SignUp.js
@@ -4,41 +4,43 @@ import axios from "axios";
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [responseMessage, setResponseMessage] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
-      const response = await axios.post("http://localhost:5000/auth/signup", { email, password });
-      console.log(response.data);
+      const response = await axios.post("http://localhost:3001/signup", {
+        email,
+        password,
+      });
+      setResponseMessage(response.data.message);
     } catch (error) {
-      console.error(error.response.data);
+      setResponseMessage("An error occurred. Please try again.");
     }
   };
-
-
 
   return (
     <div>
       <h2>Sign Up</h2>
       <form onSubmit={handleSubmit}>
-        <label>Email:</label>
         <input
           type="email"
+          placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <label>Password:</label>
         <input
           type="password"
+          placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit">Sign Up</button>
       </form>
+      {responseMessage && <p>{responseMessage}</p>}
     </div>
   );
 };
 
 export default SignUp;
-
