eb7b33faa3bc4765c86126400de76af8f39bd555
clapage <charles.lapage@gmail.com>
2023-04-08 00:52:16 +0000

    updated packages and components

--- compliment_backpack/src/index.js
+++ compliment_backpack/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = document.getElementById('root');
@@ -8,9 +8,9 @@ const root = document.getElementById('root');
 if (root !== null) {
   const app = (
     <React.StrictMode>
-      <Router>
+      <BrowserRouter>
         <App />
-      </Router>
+      </BrowserRouter>
     </React.StrictMode>
   );
 
